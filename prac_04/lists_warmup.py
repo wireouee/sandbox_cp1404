68a71785994f57044a4d9743855724c6224ddfd7
Deqian He <deqian.he@my.jcu.edu.au>
2022-04-14 16:44:30 +0800

    list practise

---
+++ prac_04/lists_warmup.py
@@ -0,0 +1,15 @@
+# 1.
+numbers = [3, 1, 4, 1, 5, 9, 2]
+numbers[0] = 10
+
+# 2.
+numbers = [3, 1, 4, 1, 5, 9, 2]
+numbers[-1] = 1
+
+# 3.
+numbers = [3, 1, 4, 1, 5, 9, 2]
+print(numbers[2:])
+
+# 4.
+numbers = [3, 1, 4, 1, 5, 9, 2]
+print(9 in numbers)
