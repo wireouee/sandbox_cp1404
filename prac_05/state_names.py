440edfdb26d825c6051422c1d95e2a360ece803d
Deqian He <deqian.he@my.jcu.edu.au>
2022-04-21 17:18:07 +0800

    code dictionary has followed PEP 8

---
+++ prac_05/state_names.py
@@ -0,0 +1,11 @@
+CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory", "WA": "Western Australia",
+                "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania"}
+print(CODE_TO_NAME)
+
+state_code = input("Enter short state: ")
+while state_code != "":
+    if state_code in CODE_TO_NAME:
+        print(state_code, "is", CODE_TO_NAME[state_code])
+    else:
+        print("Invalid short state")
+    state_code = input("Enter short state: ")
