e256c57e0354e2eb5af842641f107a6fa353f272
Deqian He <deqian.he@my.jcu.edu.au>
2022-05-16 05:25:00 +0800

    guitar_object

---
+++ prac_06/guitar.py
@@ -0,0 +1,19 @@
+current_year = 2022
+
+
+class Guitar:
+    def __init__(self, name="", year=0, cost=0):
+        self.name = name
+        self.year = year
+        self.cost = cost
+
+    def __str__(self):
+        return "{}({}) : ${:,.2f}".format(self.name, self.year, self.cost)
+
+    def get_age(self):
+        return current_year - self.year
+
+    def is_vintage(self):
+        return self.get_age() > 50
+
+
